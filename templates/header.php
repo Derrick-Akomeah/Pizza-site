bc84ac9fb46a1615dfafe11d9dc2d654b4c1cd13
Derrick Akomeah <dakomeah72@gmail.com>
2022-04-26 09:51:22 +0000

    cookies and sessions update

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,22 @@
+<?php 
+
+    session_start();
+
+  //$_SESSION['name'] = 'mario';
+
+    if($_SERVER['QUERY_STRING'] == 'noname'){
+    //unset($_SESSION['name']);
+    session_unset();
+    }
+
+  // null coalesce
+    $name = $_SESSION['name'] ?? 'Guest';
+
+      // get cookie
+    $gender = $_COOKIE['gender'] ?? 'Unknown';
+
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,6 +34,8 @@
         <div class="container">
             <a class="brand-logo brand-text" href="index.php">Kalaca Pizza</a>
             <ul id="nav-mobile"class="right hide-on-small-and-down">
+                <li class="grey-text">Hello <?php echo htmlspecialchars($name); ?></li>
+                <li class="grey-text">(<?php echo htmlspecialchars($gender); ?>)</li>
                 <li><a href="add.php" class="btn brand z-depth-0"> Add a Pizza </a></li>
                 
             </ul>
